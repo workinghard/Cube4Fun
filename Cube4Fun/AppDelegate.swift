632265df740ca7e98b90bbbed704be20751961d2
workinghard <niki1999@gmx.de>
2015-03-27 19:19:09 -0700

    Initial Commit

---
+++ Cube4Fun/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  Cube4Fun
+//
+//  Created by Nik on 27.03.15.
+//  Copyright (c) 2015 DerNik. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    @IBOutlet weak var window: NSWindow!
+    
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+    
+}
