46c028ab9e65a07b7b4bb08cec1ef75f1c1e521c
workinghard <niki1999@gmx.de>
2015-04-23 21:26:19 -0700

    a

--- Cube4Fun/src/ObjCtoCPlusPlus.mm
+++ Cube4Fun/src/ObjCtoCPlusPlus.mm
@@ -1,4 +1,9 @@
 
+//  ObjCtoCPlusPlus.mm
+//
+//  Created by Nikolai Rinas on 02/02/2015.
+//  Copyright (c) 2015 Nikolai Rinas. All rights reserved.
+//
 //  This program is free software: you can redistribute it and/or modify
 //  it under the terms of the GNU General Public License as published by
 //  the Free Software Foundation, either version 3 of the License, or
---
+++ README.md
@@ -0,0 +1,24 @@
+Cube4Fun
+==================
+
+![alt text][logo]
+
+Details coming soon ...
+
+Arduino:
+--------------------------------------
+
+...
+
+----
+
+Rainbowduino:
+--------------------------------------
+
+...
+
+----
+
+
+
+[logo]: http://cube4fun.net/public/Cube6-256j.png "Logo"
